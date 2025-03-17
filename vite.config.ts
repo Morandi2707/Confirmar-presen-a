7561ac11793c4dc84bcea4c77eb6d41526902d63
Eduardo Morandi <eduardo.morandi@BNGMETAL.CORP>
2025-03-17 08:20:00 -0300

    vite.ts concertado

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: 'public', // Define dist/assets como diretório de origem (não recomendado)
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  build: {
+    outDir: '../' // Define onde os arquivos compilados serão gerados
+  }
 });
